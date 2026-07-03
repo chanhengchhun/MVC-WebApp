97e5b0dbcb8e511d242ac5b2d3eef02a750874ed
Jey Chhun <136158401+chanhengchhun@users.noreply.github.com>
2026-07-03 18:52:57 -0400

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MVC_App_ASP.NET_Core.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
